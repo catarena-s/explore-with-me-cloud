c8794105d6412e3bd3aa2c2732239b7215ed917f
catarena-s <catarena@gmail.com>
2023-09-10 13:29:52 +0300

    refactoring

---
+++ gateway/src/main/java/dev/shvetsova/ewmc/gateway/GatewayApp.java
@@ -0,0 +1,13 @@
+package dev.shvetsova.ewmc.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApp.class, args);
+    }
+
+}
--- git-config/src/main/java/dev/shvetsova/ewmc/config/ConfigApp.java
+++ git-config/src/main/java/dev/shvetsova/ewmc/config/ConfigApp.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-public class EwmConfigApp {
+public class ConfigApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(EwmConfigApp.class, args);
+        SpringApplication.run(ConfigApp.class, args);
     }
 
 }
