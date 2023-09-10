a26364c5e02fb209925b784ce98ab1ce833404d9
catarena-s <catarena@gmail.com>
2023-09-10 13:30:43 +0300

    refactoring

--- ewm-common/src/main/java/dev/shvetsova/ewmc/common/exception/ApiException.java
+++
@@ -1,5 +0,0 @@
-package dev.shvetsova.ewmc.common.exception;
-
-public interface ApiException {
-    ApiError getApiError();
-}
--- ewm-main-service/src/main/java/dev/shvetsova/ewmc/main/service/location/LocationService.java
+++
@@ -1,9 +0,0 @@
-package dev.shvetsova.ewmc.main.service.location;
-
-import dev.shvetsova.ewmc.common.dto.location.LocationDto;
-import dev.shvetsova.ewmc.main.model.Location;
-
-public interface LocationService {
-
-    Location findLocation(LocationDto location);
-}
